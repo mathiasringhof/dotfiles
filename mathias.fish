f7f16f891c19bd657e57d8be39114adb96b91bdf
Mathias Ringhof <mathias@ringhof.eu>
2025-07-24 16:22:27 +0200

    fixed a brainfart
    Commited the symbolic links instead of the dotfiles...

--- mathias.fish
+++ mathias.fish
@@ -1,1 +1,15 @@
-/Users/mathias/.config/fish/conf.d/mathias.fish
+alias l 'eza --icons'
+alias ll 'eza -l --icons --git'
+alias la 'eza -la --icons --git'
+alias lt 'eza --tree --icons'
+
+set PATH /opt/homebrew/bin /opt/local/bin /opt/local/sbin $PATH
+
+set -gx LANG "en_US.utf-8"
+set -gx LC_ALL "en_US.utf-8"
+set -gx VISUAL nvim
+set -gx EDITOR nvim
+
+kubectl completion fish | source
+helm completion fish | source
+zoxide init fish --cmd j | source
