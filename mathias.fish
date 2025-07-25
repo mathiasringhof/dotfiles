c121cc6c42f046873218450966f03d37de9a8513
Mathias Ringhof <mathias@ringhof.eu>
2025-07-25 14:20:55 +0200

    removed fzf plugin for fzf integration

--- mathias.fish
+++ mathias.fish
@@ -13,3 +13,19 @@ set -gx EDITOR nvim
 kubectl completion fish | source
 helm completion fish | source
 zoxide init fish --cmd j | source
+
+# Preview file content using bat (https://github.com/sharkdp/bat)
+export FZF_CTRL_T_OPTS="
+  --walker-skip .git,node_modules,target,Library,crypt
+  --preview 'bat -n --color=always {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+# CTRL-Y to copy the command into clipboard using pbcopy
+export FZF_CTRL_R_OPTS="
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+  --color header:italic
+  --header 'Press CTRL-Y to copy command into clipboard'"
+# Print tree structure in the preview window
+export FZF_ALT_C_OPTS="
+  --walker-skip .git,node_modules,target,Library,crypt
+  --preview 'tree -C {}'"
+fzf --fish | source
