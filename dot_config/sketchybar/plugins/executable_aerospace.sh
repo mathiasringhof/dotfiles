073cf3d1949e4b668e43b477c7731843f2a6976b
Mathias Ringhof <mathias@ringhof.eu>
2025-08-10 20:58:53 +0200

    nice basic configuration with subtle mode highlight

--- dot_config/sketchybar/plugins/executable_aerospace.sh
+++ dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME background.drawing=on
+  sketchybar --set $NAME background.drawing=on background.color=0xff7aa2f7 label.color=0xff1a1b26 icon.color=0xff1a1b26
 else
-  sketchybar --set $NAME background.drawing=off
+  sketchybar --set $NAME background.drawing=off label.color=0xffa9b1d6 icon.color=0xffa9b1d6
 fi
