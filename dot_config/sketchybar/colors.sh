0b88804ef3c4c8b5ad7bb92d59eda884870a8ed7
Mathias Ringhof <mathias@ringhof.eu>
2025-08-11 10:44:42 +0200

    forgot to add the colors file

---
+++ dot_config/sketchybar/colors.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Tokyo Night Color Palette
+export WHITE=0xffffffff
+export BLACK=0xff000000
+export RED=0xfff7768e
+export GREEN=0xff9ece6a
+export BLUE=0xff7aa2f7
+export YELLOW=0xffe0af68
+export ORANGE=0xffff9e64
+export MAGENTA=0xffbb9af7
+export GREY=0xff565f89
+export TRANSPARENT=0x00000000
+
+# Tokyo Night Specific Colors
+export COMMENT=0xff565f89
+export STRING=0xff9ece6a
+export KEYWORD=0xff7aa2f7
+export NUMBER=0xffff9e64
+export OPERATOR=0xff89ddff
+
+# SketchyBar Color Scheme
+export BAR_COLOR=0xe616161e
+export ITEM_BG_COLOR=0x44000000
+export ACCENT_COLOR=0xff7aa2f7
+
+# Space Colors
+export SPACE_SELECTED=$GREEN
+export SPACE_UNSELECTED=$COMMENT
+export SPACE_BACKGROUND=$BLUE
+
+# Default Colors
+export ICON_COLOR=0xffa9b1d6
+export LABEL_COLOR=0xffa9b1d6
+export BORDER_COLOR=0xff3d59a1
